ddcb3ae8e088e801304faa92551503211d1ab781
ipranavprashant <ipranavprashant@gmail.com>
2023-12-14 03:14:08 +0530

    refactored frontend

--- client/src/components/jsx/RaiseADispute.jsx
+++ client/src/components/jsx/RaiseADispute.jsx
@@ -49,7 +49,7 @@ const RaiseADispute = () => {
 
                 <section className="section2 clearfix">
                     <div className="col2 column1 first">
-                        <iframe
+                        <iframe className='map'
                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3912.1796100106694!2d75.93106097504986!3d11.321579088862295!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba642fd50000001%3A0xbe8a77db953bda6c!2sNational%20Institute%20of%20Technology%2C%20Calicut!5e0!3m2!1sen!2sin!4v1702252973479!5m2!1sen!2sin"
                             title="club-location"
                             width="600"
--- client/src/components/jsx/Sponsorship.jsx
+++ client/src/components/jsx/Sponsorship.jsx
@@ -80,7 +80,7 @@ const Sponsorship = () => {
                 <p className='to-centre'>Or e-mail us at <span>&nbsp;</span><a href="mailto:enquireclub@nitc.ac.in"> enquireclub@nitc.ac.in</a></p>
                 <section className="section2 clearfix">
                     <div className="col2 column1 first">
-                        <iframe
+                        <iframe className='map'
                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3912.1796100106694!2d75.93106097504986!3d11.321579088862295!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba642fd50000001%3A0xbe8a77db953bda6c!2sNational%20Institute%20of%20Technology%2C%20Calicut!5e0!3m2!1sen!2sin!4v1702252973479!5m2!1sen!2sin"
                             title="club-location"
                             width="600"
